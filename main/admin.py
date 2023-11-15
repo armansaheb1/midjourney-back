1c2851e34be965aa9be1703befbb70ba9a4856c4
armansaheb1 <armansaheb@gmail.com>
2023-11-15 20:02:19 +0000

    Update

--- main/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Site, Link, ImagineOrder, Image, Transaction
-
-# Register your models here.
-admin.site.register(Site)
-admin.site.register(Link)
-admin.site.register(ImagineOrder)
-admin.site.register(Transaction)
-#admin.site.register(Image)
