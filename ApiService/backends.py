a76eabdf3deeb88e3628667fd92876200f326f3b
armansaheb1 <armansaheb@gmail.com>
2024-11-17 23:38:31 +0000

    Update

--- ApiService/backends.py
+++
@@ -1,6 +0,0 @@
-from rest_framework_simple_api_key.backends import APIKeyAuthentication
-from ApiService.models import OrganizationAPIKey
-
-
-class OrganizationAPIKeyAuthentication(APIKeyAuthentication):
-    model = OrganizationAPIKey
--- routers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import routers
-
-from ApiService.viewsets import OrganizationViewSet
-
-router = routers.SimpleRouter()
-router.register("organization", OrganizationViewSet, basename="organization")
-
-urlpatterns = router.urls
